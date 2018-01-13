2ba6b5e94c5b11399b6f02a8fb6a385168e8acf5
Kyle Harris <k_harris95@live.com>
2018-01-13 05:02:22 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,8 +25,7 @@ def won?(board)
     pos_2 = board[win_index_2]
     pos_3 = board[win_index_3]
 
-    all_pos = [win_index_1, win_index_1, win_index_1].all?{|index| position_taken?(board, index)}
-    puts "#{win_combination}, #{all_pos}"
+    all_pos = [win_index_1, win_index_1, win_index_1].all?{|index| puts"#{position_taken?(board, index)}""}
   end
 end
 
