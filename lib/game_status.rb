6fb0093ceaebdd518d989cb9925ec9df99437336
Kyle Harris <k_harris95@live.com>
2018-01-13 02:46:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,17 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
+  [6,7,8], #Bottom row
+  [0,3,6], #First column
+  [1,4,7], #Middle columns
+  [2,5,8], #Last columns
+  [0,4,8], #Diagnol
+  [2,4,6] #Diagnol
+]
+
+def won?(board)
+  
+end
