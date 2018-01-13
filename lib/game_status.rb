90a4defa4680e6ec9003b8c6f806c0f06700d73a
Kyle Harris <k_harris95@live.com>
2018-01-13 04:46:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,11 +27,5 @@ def won?(board)
     all_pos = [pos_1, pos_2, pos_3].each do |pos|
       position_taken?(board, pos)
     end
-
-    if all_pos
-      return true
-    else
-      return false
-    end
   end
 end
